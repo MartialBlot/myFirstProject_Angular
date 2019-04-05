114068031cc88c89fc91f17a18d9695fac852ac4
BLOT Martial <martial.blot@outlook.fr>
2019-04-05 13:43:44 +0200

    Angular Template-Forms

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+export class Order {
+  user: string;
+  email: string;
+}
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
+
 export class SignUpComponent implements OnInit {
-  email= 'Entrer votre mail'
+
+  model: Order = new Order();
+
   constructor() { }
+
+  onSubmit() {
+  }
 
   ngOnInit() {
   }
